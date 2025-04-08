54c48ed759f1a9b3d4a528fb07d98b909009553a
vmumbi653 <[mainamumbi23@gmail.com]>
2025-04-08 10:58:26 +0300

    CHANGE THE FUNCTION THAT ADD BOOK TO LIBRARY ARRAY
    I have changed the function that is meant to add new books to the array. The input values from the form are now being added to the array myLibrary.

--- javascript.js
+++ javascript.js
@@ -1,93 +1,104 @@
 //create empty array to store book objects
 let myLibrary = [];
 
+const newBookBtn = document.getElementById('newBook');
+const dialog = document.getElementById('dialog');
+const submitBtn = document.getElementById('submit');
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+
+
+
 //create book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title =  title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
     this.id = crypto.randomUUID(); //add random UUID to each book
-}
+  
 
-Book.prototype.addBookToLibrary = function() {
+}
+function addBookToLibrary() {
+
+    let newBook = new Book(title.value, author.value, pages.value);
     //push book object itself to array
-   myLibrary.push(this);
+   myLibrary.push(newBook);
 }
 
-const book1 = new Book ('The Bat', 'Jo Nesbo', '425', 'read');
-const book2 = new Book ('Purple Panties', 'Zane', '200', 'Not read');
-const book3 = new Book ('The Kite Runner', 'Khaled Hossein', '371', 'read');
-const book4 = new Book ('The Litigators', 'John Grisham', '470', 'not read');
-const book5 = new Book ('The Killing', 'David Hewson', '707', 'read');
-const book6 = new Book ('The Killing II', 'David Hewson', '539', 'read');
 
-book1.addBookToLibrary();
-book2.addBookToLibrary();
-book3.addBookToLibrary();
-book4.addBookToLibrary();
-book5.addBookToLibrary();
-book6.addBookToLibrary();
+const book1 = new Book ('The Bat', 'Jo Nesbo', '425');
+// const book2 = new Book ('Purple Panties', 'Zane', '200', 'Not read');
+// const book3 = new Book ('The Kite Runner', 'Khaled Hossein', '371', 'read');
+// const book4 = new Book ('The Litigators', 'John Grisham', '470', 'not read');
+// const book5 = new Book ('The Killing', 'David Hewson', '707', 'read');
+// const book6 = new Book ('The Killing II', 'David Hewson', '539', 'read');
+
+myLibrary.push(book1);
+// book2.addBookToLibrary();
+// book3.addBookToLibrary();
+// book4.addBookToLibrary();
+// book5.addBookToLibrary();
+// book6.addBookToLibrary();
 
 //test new book object is stored in array//
 console.log(myLibrary);
 
 const container = document.querySelector('.card-container');
-
-// function displayBooks() {
-//    const card =  document.createElement('div');
-//    card.classList.add('card');
-//    container.appendChild(card);
-
-// }
-
-//for loop function
-// for(let i=0; i<=myLibrary.length; i++) {
-//     console.table(myLibrary);
-// }
-
-// function displayBooks() {
-// myLibrary.forEach((myLibrary) => {
-//     const card = document.createElement('div');
-//     card.classList.add('card');
-//     container.appendChild(card);
-//     card.textContent = myLibrary.toString();
-
-// });
-// }
-
-// displayBooks();
-
-    // for(let i=0; i<myLibrary.length; i++) {
-    //         const card = document.createElement('div');
-    //         card.classList.add('card');
-    //         card.textContent = `${myLibrary[i].title}; ${myLibrary[i].author}; ${myLibrary[i].pages}; ${myLibrary[i].read}`;
-    //         container.appendChild(card);
-    // }
+let content;
 
 
-    myLibrary.forEach((book) => {
+    myLibrary.forEach((newBook) => {
         const card = document.createElement('div');
         card.classList.add('card');
 
             //creeate a multi-line string called content that containes the HTML Markup for each card layout
-        const content = `
+         content = `
             <div class = 'card-body'>
-              <h4>${book.title}</h4>
-              <p>${book.author}</p>
-              <p>${book.pages}</p>
-              <p>${book.read}</p>
+             <h4> ${newBook.title}</4>
+              <p>${newBook.author}</p>
+              <p>${newBook.pages}</p>
             </div>
 
            `;
            container.innerHTML += content;
-    });
 
-    const newBookBtn = document.getElementById('newBook');
-    const dialog = document.getElementById('dialog');
-    const submitBtn = document.getElementById('submit');
+    });
 
     //button shows the dialog modally
     newBookBtn.addEventListener("click", () => {
         dialog.showModal();
+    });
+
+
+    
+//    function submitWorks(event) {
+//     event.preventDefault();
+//     const form = document.getElementById('form');
+
+    const form = document.getElementById('form');
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        addBookToLibrary();
+
+        dialog.close();
+
     })
+
+
+//create a remove button
+  const cardBodies = document.getElementsByClassName('card-body');
+
+  Array.from(cardBodies).forEach((cardBody) => {
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('removeBtn');
+    removeBtn.textContent = "Remove";
+    cardBody.appendChild(removeBtn);
+
+  });
+
+  const removeBtns = document.querySelectorAll('.removeBtn');
+
+  
